c46d6cdf37d3e442f580866826fa3c6320f8c9de
Vide0Master <signatov2134@gmail.com>
2024-10-16 14:05:54 +0300

    updated start bat

--- start.bat
+++ start.bat
@@ -1,3 +1,12 @@
 @echo off
+:START
+echo Attempt to update PI-Archive...
+git pull
+
+echo Checking libraries...
+call npm install || pause
+
+echo Starting service...
 node index.js
+goto START
 pause
