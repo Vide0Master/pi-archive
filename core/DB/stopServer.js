bd587438f26ed6393b5ac54839d3e73add19c69c
Vide0Master <signatov2134@gmail.com>
2024-10-16 15:49:08 +0300

    patch

---
+++ core/DB/stopServer.js
@@ -0,0 +1,10 @@
+
+//создание пользователя
+
+const syscontroller = require('../systemController.js')
+
+module.exports = (db) => {
+    return new Promise(async resolve => {
+        process.exit(0);
+    })
+}
