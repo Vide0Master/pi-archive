e7489c1e8b3a928b7eafe14608097decc6807eae
Vide0Master <signatov2134@gmail.com>
2024-11-29 03:34:31 +0200

    fast stupid patch

--- core/DB/getPostsCount.js
+++ core/DB/getPostsCount.js
@@ -1,10 +1,10 @@
 const SysController = require('../systemController');
 
-module.exports = (db, queryText) => {
+module.exports = (db, queryText, override) => {
     return new Promise((resolve) => {
         let query = 'SELECT COUNT(*) as count';
 
-        const { Cquery, params } = SysController.queryConstructor(queryText, undefined, undefined, true)
+        const { Cquery, params } = SysController.queryConstructor(queryText, undefined, undefined, override)
 
         db.get(query + Cquery, params, (err, row) => {
             if (err) SysController.log('e/' + err)
